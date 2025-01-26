116827668304fcc275fe40371821f5d402625da4
Jorge <jararze@gmail.com>
2025-01-26 17:23:18 -0400

    para ultima practica

---
+++ src/screens/Login.jsx
@@ -0,0 +1,11 @@
+import FormLoginWithMotion from "../Components/Forms/FormLoginWithMotion.jsx";
+
+const Login = () => {
+    return (
+        <>
+            <FormLoginWithMotion titleForm="Welcome to FullStack program" />
+        </>
+    );
+};
+
+export default Login;
